2d4f794e0f97e2c9107a527dfa90825c31e54462
ridvanasci <asci.ridvan@gmail.com>
2024-08-15 18:46:46 +0300

    Click Me Message. When pressed "hello World" displayed. Working. Build succesful

---
+++ src/main/java/com/example/vaadinone/HelloWorldUI.java
@@ -0,0 +1,16 @@
+package com.example.vaadinone;
+
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.server.VaadinRequest;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+//@Route("")
+public class HelloWorldUI extends VerticalLayout {
+    public HelloWorldUI() {
+        System.out.println("HelloWorldUI");
+    }
+HelloWorldUI a = new HelloWorldUI();
+}
